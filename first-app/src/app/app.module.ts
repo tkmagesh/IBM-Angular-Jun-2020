f6282ce9beb8135c5e80e2b5add7529748ffa4cd
Magesh Kuppan <tkmagesh77@gmail.com>
2020-06-23 17:13:30 +0530

    Calculator in Angular

--- first-app/src/app/app.module.ts
+++ first-app/src/app/app.module.ts
@@ -2,15 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CalculatorComponent } from './calculator/calculator.component';
 
+/* 
+  declarations - register all the UI entities of the application (Components, Pipes, Directives)
+  providers - register the NON-UI entities of the application ( services )
+  imports - register the other Angular modules that this module depends on
+  bootstrap - top level components in the app (index.html)
+
+*/
 @NgModule({
   declarations: [
     AppComponent
+    , CalculatorComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    /* AppComponent, */
+    CalculatorComponent
+  ]
 })
 export class AppModule { }
